8c6daa85be85ac1bbcb504b79dc815fdeaf83a5b
Mazafard <mazafard@gmail.com>
2023-02-04 07:24:26 +0000

    add url to private groups

--- src/components/user-profile-head-stats.jsx
+++ src/components/user-profile-head-stats.jsx
@@ -2,10 +2,13 @@ import { Link } from 'react-router';
 import cn from 'classnames';
 
 import { useSelector } from 'react-redux';
+
+import { faCamera } from '@fortawesome/free-solid-svg-icons/faCamera';
 import { pluralForm } from '../utils';
 import styles from './user-profile-head.module.scss';
 import TimeDisplay from './time-display';
 import UserName from './user-name';
+import { Icon } from './fontawesome-icons';
 
 export const UserProfileHeadStats = ({ user, canFollowStatLinks }) => {
   const invitedBy = useSelector((state) => state.invitedByMap[user.username]);
@@ -61,6 +64,12 @@ export const UserProfileHeadStats = ({ user, canFollowStatLinks }) => {
               <TimeDisplay inline timeStamp={parseInt(createdAt)} absolute dateOnly />
             </span>
           </li>
+          <IconLink
+            icon={faCamera}
+            title="  Group Call"
+            href={`https://meet.freefeed.me/${username}`}
+            canFollow={canFollowStatLinks}
+          />
         </ul>
       </div>
     );
@@ -136,6 +145,24 @@ function StatLink({ value, title, linkTo, canFollow, className }) {
       <Link to={linkTo} className={styles.statlinkText}>
         {content}
       </Link>
+    );
+  } else {
+    content = <span className={styles.statlinkText}>{content}</span>;
+  }
+
+  return <li className={cn(styles.statlink, className)}>{content}</li>;
+}
+
+function IconLink({ icon, title, href, canFollow, className }) {
+  let content;
+  content = ` ${title}`;
+
+  if (canFollow) {
+    content = (
+      <a className={styles.searchButton} href={href}>
+        <Icon icon={icon} className={styles.caret} />
+        {`  Group Call`}
+      </a>
     );
   } else {
     content = <span className={styles.statlinkText}>{content}</span>;
